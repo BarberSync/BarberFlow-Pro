b37f4f95f1eb1e3802bb072ea96f8d6175f2d758
BarberSync <sync.booking.services@gmail.com>
2026-03-15 05:06:12 +0000

    Update firebase-init.js

--- js/modules/firebase-init.js
+++ js/modules/firebase-init.js
@@ -1,9 +1,3 @@
-// ملف ربط موقع barberflow-pro مع قاعدة بيانات firebase
-
-
-// js/modules/firebase-init.js
-
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
 
@@ -17,26 +11,12 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-s
 
 
 const firebaseConfig = {
-
-
     apiKey: "AIzaSyB8s5Qjcoo45cxloelyYP181l_TZWsXaCw",
-
-
     authDomain: "barberflow-v2-c90d8.firebaseapp.com",
-
-
     projectId: "barberflow-v2-c90d8",
-
-
     storageBucket: "barberflow-v2-c90d8.firebasestorage.app",
-
-
     messagingSenderId: "599133211849",
-
-
     appId: "1:599133211849:web:f9ec18973417432d439118"
-
-
 };
 
 
@@ -52,7 +32,5 @@ const auth = getAuth(app);
 const storage = getStorage(app);
 
 
-// إضافة التصدير لتصبح الخدمات متاحة في بقية الملفات
-
-
+// التصحيح هنا: إضافة export لجعل الخدمات متاحة
 export { db, auth, storage };

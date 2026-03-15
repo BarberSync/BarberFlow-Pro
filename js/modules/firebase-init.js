788f05ea1c81dd0b6bc0453dde10542ba231af5b
BarberSync <sync.booking.services@gmail.com>
2026-03-15 02:29:57 +0000

    Update firebase-init.js

--- js/modules/firebase-init.js
+++ js/modules/firebase-init.js
@@ -1,1 +1,59 @@
 //ملف ربط موقع barberflow-pro مع قاعدة بيانات firebase
+
+
+
+
+// js/modules/firebase-init.js
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+
+import { getFirestore, collection, addDoc, getDocs, query, where, updateDoc, doc, arrayUnion, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+
+const firebaseConfig = {
+
+
+    apiKey: "AIzaSyB8s5Qjcoo45cxloelyYP181l_TZWsXaCw",
+
+
+    authDomain: "barberflow-v2-c90d8.firebaseapp.com",
+
+
+    projectId: "barberflow-v2-c90d8",
+
+
+    storageBucket: "barberflow-v2-c90d8.firebasestorage.app",
+
+
+    messagingSenderId: "599133211849",
+
+
+    appId: "1:599133211849:web:f9ec18973417432d439118"
+
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore(app);
+
+
+const auth = getAuth(app);
+
+
+const storage = getStorage(app);
+
+
+export { auth, db, storage, collection, getDocs, doc, setDoc, updateDoc };
+
+

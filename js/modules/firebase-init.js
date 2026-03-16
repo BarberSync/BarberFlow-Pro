32e864b1eaba52dc7004366f17513b49aed4d7a3
BarberSync <sync.booking.services@gmail.com>
2026-03-16 01:16:25 +0000

    Update firebase-init.js

--- js/modules/firebase-init.js
+++ js/modules/firebase-init.js
@@ -1,3 +1,9 @@
+// js/modules/firebase-init.js
+
+
+/* --- 1. استيراد وظائف التهيئة من مكتبات Firebase الرسمية --- */
+
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
 
@@ -10,14 +16,34 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 
+/* --- 2. إعدادات مشروع Firebase الخاصة بك --- */
+
+
 const firebaseConfig = {
+
+
     apiKey: "AIzaSyB8s5Qjcoo45cxloelyYP181l_TZWsXaCw",
+
+
     authDomain: "barberflow-v2-c90d8.firebaseapp.com",
+
+
     projectId: "barberflow-v2-c90d8",
+
+
     storageBucket: "barberflow-v2-c90d8.firebasestorage.app",
+
+
     messagingSenderId: "599133211849",
+
+
     appId: "1:599133211849:web:f9ec18973417432d439118"
+
+
 };
+
+
+/* --- 3. تهيئة التطبيق واستخراج الخدمات البرمجية --- */
 
 
 const app = initializeApp(firebaseConfig);
@@ -32,5 +58,7 @@ const auth = getAuth(app);
 const storage = getStorage(app);
 
 
-// التصحيح هنا: إضافة export لجعل الخدمات متاحة
+/* --- 4. تصدير الخدمات لاستخدامها في باقي الملفات --- */
+
+
 export { db, auth, storage };

14fea285e3a81e82abb8a169347305d5279ceb5b
BarberSync <sync.booking.services@gmail.com>
2026-03-16 05:08:44 +0000

    Update salons.js

--- js/salons.js
+++ js/salons.js
@@ -10,7 +10,7 @@ import { db } from './modules/firebase-init.js';
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 
-import { createSalonCard } from './salonCard.js';
+import { createSaloncard } from './salonCard.js';
 
 
 /* --- 2. دالة جلب وعرض الصالونات من Firestore --- */

13bb17de2b588f53105eae0f6ad076d84d0e38c8
BarberSync-Admin <contact@barbersync.com>
2026-03-17 03:27:41 +0000

    إعدادات المان

--- js/modules/firebase-init.js
+++ js/modules/firebase-init.js
@@ -1,28 +1,17 @@
-// js/modules/firebase-init.js
-
-
-/* --- 1. استيراد وظائف التهيئة من مكتبات Firebase الرسمية --- */
+/* --- 1. استيراد وظائف التهيئة --- */
 
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-
-
-/* --- 2. إعدادات مشروع Firebase الخاصة بك --- */
+/* --- 2. الإعدادات المحمية --- */
 
 
 const firebaseConfig = {
 
 
-    apiKey: "AIzaSyB8s5Qjcoo45cxloelyYP181l_TZWsXaCw",
+    // قراءة المفتاح من ملف .env المخفي
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
 
 
     authDomain: "barberflow-v2-c90d8.firebaseapp.com",
@@ -43,22 +32,4 @@ const firebaseConfig = {
 };
 
 
-/* --- 3. تهيئة التطبيق واستخراج الخدمات البرمجية --- */
-
-
 const app = initializeApp(firebaseConfig);
-
-
-const db = getFirestore(app);
-
-
-const auth = getAuth(app);
-
-
-const storage = getStorage(app);
-
-
-/* --- 4. تصدير الخدمات لاستخدامها في باقي الملفات --- */
-
-
-export { db, auth, storage };
